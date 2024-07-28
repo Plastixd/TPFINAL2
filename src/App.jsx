ef30b72a3e885739575a1384ac181fd1428caaa1
Pedro <herrerapedro2002@gmail.com>
2024-07-28 20:26:38 -0300

    misc necesario agregado y componente skills recibe info del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,33 @@ import Footer from "./Components/Footer";
 
 function App() {
 
+  const Skillsdata = [{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },{
+    title: "HTML5",
+    img: "./public/html5.svg",
+    text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
+  },
+]
+
   return (
     <section>
       <h1>HOLA MUNDO</h1>
@@ -18,7 +45,7 @@ function App() {
       <h2>Speciality</h2>
       <Speciality/>
       <h2>Skills</h2>
-      <Skills/>
+      {Skillsdata.map(elemento=><Skills title={elemento.title} img={elemento.img} text={elemento.text}/>)}
       <Subskills/>
       <h2>Portfolio</h2>
       <Portfolio/>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
+
 import PropTypes from 'prop-types'
 
-function Skills(props) {
+function Skills({title,img,text}) {
   return (
-    <section>
-        <div>
-            <img src="" alt="" />
-            <h2>HTML5</h2>
-        </div>
-        <p>
-            Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.
-        </p>
-    </section>
+    <div>
+      <h2>{text}</h2>
+    </div>
   )
 }
 
