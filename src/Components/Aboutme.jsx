96e9ed57ea0385826c7067232d8191279f155ede
Pedro <herrerapedro2002@gmail.com>
2024-07-30 12:29:04 -0300

    componente Aboutme posicionado

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -1,44 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Box, Flex, Text } from '@chakra-ui/react';
 
-function Aboutme(props) {
+
+function Aboutme() {
   return (
     <section>
+    <Box pt="50px" pb="200px" width="88%"border="1px" borderColor="black">
+        
+        <Box align="left" border="1px" borderColor="red">
         <h2>Algo</h2> <h2>Sobre</h2> <h2>Mi</h2>
-
-        <div>
+        </Box>
+            <Text align="left" mr="80px" ml="82px">
             <p>¡Hola! Mi nombre es Pedro y estoy cursando la diplomatura en programación web full stack 2024. He completado cursos como DBA1 de la academia CISCO en la UTN y demás. En este momento, estoy estudiando y profundizando en el desarrollo web. Soy estudiante en la Universidad Tecnológica, estudiando la carrera sistemas.</p>
-            <br />
-            <br />
             <p>Creo que lo más importante en la vida es encontrarse a uno mismo y descubrir lo que te gusta. Estoy muy contento de haber elegido este camino. Esto me motiva y por eso sigo adelante. Paciencia y un poco de esfuerzo.</p>    
-        </div>
-  
-        <section>
-            <div>
-                <h2>Que</h2>
-                <h2>Puedo</h2>
-                <h2>Ofrecer</h2>
-                <h2>A ti</h2>
-            </div>
-            <div>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <p>Trabajar conmigo significa recibir un trabajo de calidad. Doy lo mejor de mí y entiendo bien al cliente o empleador. ¡Además, aprendo y me adapto rápidamente!</p>
-            </div>
-        </section>
+            </Text>
+
+        <Box align="right" border="1px" borderColor="red">
+            <h2>Que</h2>
+            <h2>Puedo</h2>
+            <h2>Ofrecer</h2>
+            <h2>A ti</h2>
+        </Box>
+            <Text align="left" ml="82px">
+            <p>Trabajar conmigo significa recibir un trabajo de calidad. Doy lo mejor de mí y entiendo bien al cliente o empleador. ¡Además, aprendo y me adapto rápidamente!</p>
+            </Text>
+    </Box>
+    
     </section>
-  )
-}
+        )
+    }
 
-Aboutme.propTypes = {
 
-}
 
 export default Aboutme
 
