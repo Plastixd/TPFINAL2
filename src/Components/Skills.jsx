ded243f839f73859de9da5cd4f44431a700b5ade
Pedro <herrerapedro2002@gmail.com>
2024-07-31 00:10:42 -0300

    editado path de imagenes en componente skills y cambiado informacion del index.html

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,47 +7,47 @@ function Skills() {
 
 const Skillsdata = [{
     title: "HTML5",
-    img: "./public/html5.svg",
+    img: "/html5.svg",
     text: "Desde el año 2015, mi segundo año de secundaria, conozco HTML. Es mi primer acercamiento a la programación y ciertamente le guardo aprecio a HTML por esta razón. A pesar de ser cada vez menos utilizado, considero tener conocimientos suficientes en este hipertexto.",
     id: "1",
     },{
     title: "CSS",
-    img: "./public/css.svg",
+    img: "/css.svg",
     text: "Sobre CSS puedo trabajar para estilizar el contenido de una página, valiéndome de funciones y atributos varios; como flex que creo manejar bien, animaciones para una página, y demás.",
     id: "2",
     },{
     title: "PHP",
-    img: "./public/php.svg",
+    img: "/php.svg",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati dolore quos earum ex vel mollitia dolor exercitationem illum, facere doloremque ducimus blanditiis totam repellat consequatur placeat, quisquam quo, nam corporis.",
     id: "3",
     },{
     id: "4",
     title: "Python",
-    img: "./public/py.svg",
+    img: "/py.svg",
     text: "Durante el primer año de cursado en la carrera de Ingeniería en Sistemas de la Información, en la materia Algoritmos y estrucutra de datos, el lenguaje que utilizamos para programar es Python, estoy muy familiarizado desde ese momento con Python.",
     },{
     id: "5",
     title: "Java Script",
-    img: "./public/js.svg",
+    img: "/js.svg",
     text: "Varias veces incursioné en el aprendizaje de Java Script, y a pesar de tener los conocimientos básicos, puedo utilizar esta información para complementar algún sitio web. En el curso de este año aprenderé más sobre esto."
     },{
         id: "6",
     title: "SQL",
-    img: "./public/SQL.svg",
+    img: "/SQL.svg",
     text: "Sobre SQL solo realicé un curso de DBA1, en el año 2023. Aprendí lo básico sobre bases de datos relacionales, y cómo trabajar sobre ellas. La mayoría del conocimiento que tengo en SQL es gracias a mi trabajo."
     }]
 
 const Subskillsdata = [{
     id: 1,
-    img: "./figma.svg",
+    img: "/figma.svg",
     text: "Figma"
 },{
     id: 2,
-    img: "./PS.svg",
+    img: "/PS.svg",
     text: "Adobe Photoshop"
 },{
     id: 3,
-    img: "./XD.svg",
+    img: "/XD.svg",
     text: "Adobe XD"
 }]
 
